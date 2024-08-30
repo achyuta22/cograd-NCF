6d80dd6d04ac527917611fff7e0e34c3c1412b13
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2024-08-30 23:28:12 +0530

    feat: Add Approach component and Approach1 subcomponent for approach section

--- src/App.js
+++ src/App.js
@@ -5,18 +5,17 @@ import Culture from "./opening/Culture";
 import HistoryPage from "./opening/History";
 import Hero from "./opening/Hero";
 import Navbar from "./opening/Navbar";
-// import Approach from "./opening/Approach";
+import Approach from "./opening/Approach";
 function App() {
   return (
     <Router>
-      <Navbar/>
+      <Navbar />
       <Routes>
         <Route path="/cct" element={<CCT />} />
         <Route path="/culture" element={<Culture />} />
         <Route path="/history" element={<HistoryPage />} />
         <Route path="/" element={<Hero />} />
-        // <Route path="/approach" element={<Approach />} />
-
+        <Route path="/approach" element={<Approach />} />
       </Routes>
     </Router>
   );
---
+++ src/opening/Approach.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Approach1 from "../components/Approach1";
+
+const Approach = () => {
+  return (
+    <div>
+      <Approach1 />
+    </div>
+  );
+};
+
+export default Approach;
