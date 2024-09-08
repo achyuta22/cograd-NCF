bf9b40916b853e49e6deed031dd3c5664646e61d
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2024-09-09 00:05:37 +0530

    refactor: Add routes for approach pages

--- src/App.js
+++ src/App.js
@@ -10,6 +10,10 @@ import UserPage from "./opening/User";
 import HistoryPage from "./opening/History";
 import CardsPage from "./opening/sectione";
 import Sectione1 from "./opening/sectione1";
+import Approach1 from "./components/Approach1";
+import Stages from "./components/Stages";
+import Approach2 from "./components/Approach2";
+import Approach3 from "./components/Approach3";
 function App() {
   return (
     <Router>
@@ -23,9 +27,14 @@ function App() {
         <Route path="/" element={<Hero />} />
         <Route path="/school-subjects" element={<SchoolSubjects />} />
         <Route path="/years" element={<QuizPage />} />
+        <Route path="/approach-aim" element={<Approach1 />} />
+        <Route path="/approach-stage" element={<Stages />} />
+        <Route path="/approach-learning" element={<Approach2 />} />
+        <Route path="/approach-assessment" element={<Approach3 />} />
+
         {/* <Route path="/approach" element={<Approach />} /> */}
         <Route path="/sectione" element={<CardsPage />} />
-        <Route path="/sectione1" element={<Sectione1/>} />
+        <Route path="/sectione1" element={<Sectione1 />} />
       </Routes>
     </Router>
   );
