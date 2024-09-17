33d941293686170d0c38f21c7ac074cb574b560b
bharatravihal <ravihalbharat@gmail.com>
2024-09-17 15:06:26 +0530

    image changed

--- src/sectione/sectione2-environmntLearning.jsx
+++ src/sectione/sectione2-environmntLearning.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import bgImage from '../images/vision.png'; // Update the path to your image
+import bgImage from "../images/Screenshot_2024_0817_073510.png"; // Update the path to your image
 import { useNavigate } from 'react-router-dom';
 
 const EnvironmentLearning = () => {
