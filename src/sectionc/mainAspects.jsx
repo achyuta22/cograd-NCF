1b416bab614bc4f61f517fa64e9f14e5e6be08fa
bharatravihal <ravihalbharat@gmail.com>
2024-09-17 14:53:15 +0530

    m

--- src/sectionc/mainAspects.jsx
+++ src/sectionc/mainAspects.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import image1 from "../images/Screenshot_2024_0817_073126.png"; // Replace with actual image paths
-import image2 from "../images/Screenshot_2024_0817_073216.png";
-import image3 from "../images/Screenshot_2024_0817_073243.png";
-// import image4 from "../images/Screenshot_2024_0817_073326.png"; // Add additional images
-// import image5 from "../images/Screenshot_2024_0817_073416.png";
-// import image6 from "../images/Screenshot_2024_0817_073526.png";
-// import image7 from "../images/Screenshot_2024_0817_073626.png";
-// import image8 from "../images/Screenshot_2024_0817_073726.png";
-// import image9 from "../images/Screenshot_2024_0817_073826.png";
-// import image10 from "../images/Screenshot_2024_0817_073926.png";
+import image1 from "../images/c1.jpg"; // Replace with actual image paths
+import image2 from "../images/c2.jpg";
+import image3 from "../images/c3.jpg";
+ import image4 from "../images/c4.jpg"; // Add additional images
+ import image5 from "../images/c5.jpg";
+import image6 from "../images/c6.jpg";
+ import image7 from "../images/c7.jpg";
+ import image8 from "../images/c8.jpg";
+ import image9 from "../images/c9.jpg";
+ import image10 from "../images/c10.jpg";
 import { Link } from "react-router-dom";
 
 const MainAspects = () => {
@@ -81,7 +81,7 @@ const MainAspects = () => {
           {/* Card 4 */}
           <div
             className="relative bg-cover bg-center rounded-lg shadow-lg p-6"
-            style={{ backgroundImage: `url(${image3})`, height: "150px" }}
+            style={{ backgroundImage: `url(${image4})`, height: "150px" }}
           >
             <a
               href="https://drive.google.com/file/d/1C1a4bxlIppYqrhaLBUqzy3Ouq8hoGFx8/view?usp=drive_link"
@@ -100,7 +100,7 @@ const MainAspects = () => {
           {/* Card 5 */}
           <div
             className="relative bg-cover bg-center rounded-lg shadow-lg p-6"
-            style={{ backgroundImage: `url(${image3})`, height: "150px" }}
+            style={{ backgroundImage: `url(${image5})`, height: "150px" }}
           >
             <a
 href="https://drive.google.com/file/d/1YQ658QfLz0fIPHGNkQtixE-KoBVb_2-P/view?usp=drive_link"              target="_blank"
@@ -118,7 +118,7 @@ href="https://drive.google.com/file/d/1YQ658QfLz0fIPHGNkQtixE-KoBVb_2-P/view?usp
           {/* Card 6 */}
           <div
             className="relative bg-cover bg-center rounded-lg shadow-lg p-6"
-            style={{ backgroundImage: `url(${image3})`, height: "150px" }}
+            style={{ backgroundImage: `url(${image6})`, height: "150px" }}
           >
             <a
 href="https://drive.google.com/file/d/1ikcPWd1s0SykkKkI1Y6z7bjMlhAhnEVE/view?usp=drive_link"              target="_blank"
@@ -136,7 +136,7 @@ href="https://drive.google.com/file/d/1ikcPWd1s0SykkKkI1Y6z7bjMlhAhnEVE/view?usp
           {/* Card 7 */}
           <div
             className="relative bg-cover bg-center rounded-lg shadow-lg p-6"
-            style={{ backgroundImage: `url(${image1})`, height: "150px" }}
+            style={{ backgroundImage: `url(${image7})`, height: "150px" }}
           >
             <a
 href="https://drive.google.com/file/d/1DrwoklcSIYyaEqEaCkI3IxscDGjwM3Jw/view?usp=drive_link"              target="_blank"
@@ -154,7 +154,7 @@ href="https://drive.google.com/file/d/1DrwoklcSIYyaEqEaCkI3IxscDGjwM3Jw/view?usp
           {/* Card 8 */}
           <div
             className="relative bg-cover bg-center rounded-lg shadow-lg p-6"
-            style={{ backgroundImage: `url(${image3})`, height: "150px" }}
+            style={{ backgroundImage: `url(${image8})`, height: "150px" }}
           >
             <a
 href="https://drive.google.com/file/d/1EZxcXcY3EsqE7Y29emhV-zxxxTBhVgoQ/view?usp=drive_link"              target="_blank"
@@ -172,7 +172,7 @@ href="https://drive.google.com/file/d/1EZxcXcY3EsqE7Y29emhV-zxxxTBhVgoQ/view?usp
           {/* Card 9 */}
           <div
             className="relative bg-cover bg-center rounded-lg shadow-lg p-6"
-            style={{ backgroundImage: `url(${image3})`, height: "150px" }}
+            style={{ backgroundImage: `url(${image9})`, height: "150px" }}
           >
             <a
 href="https://drive.google.com/file/d/1YgwAZSfbr9W6BrVyE9fjNdjo0X938smC/view?usp=drive_link"              target="_blank"
@@ -190,7 +190,7 @@ href="https://drive.google.com/file/d/1YgwAZSfbr9W6BrVyE9fjNdjo0X938smC/view?usp
           {/* Card 10 */}
           <div
             className="relative bg-cover bg-center rounded-lg shadow-lg p-6"
-            style={{ backgroundImage: `url(${image3})`, height: "150px" }}
+            style={{ backgroundImage: `url(${image10})`, height: "150px" }}
           >
             <a
 href="https://drive.google.com/file/d/1zwAGbuMLKZF6fZwtOZ6Be1pqdTFFQduA/view?usp=drive_link"              target="_blank"
